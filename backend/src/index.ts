f522993de9577e65c6d9f609d40745050ea854c7
Genrey <109770981+Genrei123@users.noreply.github.com>
2025-04-16 17:39:24 +0800

    Added / route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,11 @@ import 'dotenv/config'
 const app = express();
 const PORT = process.env.PORT;
 
+app.get("/", (req, res) => {
+    res.send("Hello, world!");
+
+});
+
 app.listen(PORT, () => {
     console.log(`Listening to port ${PORT}`);
 });
