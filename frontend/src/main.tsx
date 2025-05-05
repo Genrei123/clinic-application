7c2e41686cb49fb4d7eb6b933a6714cf8fb306dd
Jhon Emerwin <jhon.emerwin05@gmail.com>
2025-05-05 17:46:12 +0800

    frontend/ProgressBar
    i have made a progress bar for frontend with the baby rolls when progressing, please do improve, modify and refine it if needed, i also installed a package called framer motion

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import ErrorModal from './components/ErrorModal.tsx'
-import SuccessModal from './components/SuccessModal.tsx'
-import Button from './components/Button.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
