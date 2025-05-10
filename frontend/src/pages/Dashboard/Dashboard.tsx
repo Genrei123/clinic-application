239817d7eba6ea0f6fea0c209c190dedd6bdec7b
Kurosawa-Z <gianhiginofungo@gmail.com>
2025-05-10 22:52:05 +0800

    frontend/sidebar
    > added a sidebar for the dsahboard
    > add a responsiveness for the page
    > added a hamburger button for mobile view

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,14 @@
+import React from 'react';
+import { Sidebar } from "../../components/Sidebar"
+
 const Dashboard: React.FC = () => {
+
     return (
-        <>
-            <h1>Welcome to Dashboard!</h1>
-        </>
-    )
-}
+        <div className="bg-[#0A0E15] h-screen text-white flex justify-center items-center">
+        <Sidebar/>
+            <h1>Welcome to the Dashboard</h1>
+        </div>
+    );
+};
 
 export default Dashboard;
